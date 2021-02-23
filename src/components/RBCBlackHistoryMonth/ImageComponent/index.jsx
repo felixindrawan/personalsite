0262af7e1b638a7fa2ba53a77b9cd19ee1cae731
Felix Indrawan <felix.indrawan@mail.utoronto.ca>
2021-02-23 17:15:23 -0500

    redirects

--- src/components/RBCBlackHistoryMonth/ImageComponent/index.jsx
+++ src/components/RBCBlackHistoryMonth/ImageComponent/index.jsx
@@ -3,6 +3,7 @@ import { Box, Modal, Typography, Grid } from '@material-ui/core';
 
 import './style.css';
 
+// eslint-disable-next-line react/prop-types
 const ImageComponent = ({ image, imageName }) => {
   const [open, setOpen] = React.useState(false);
 
@@ -26,12 +27,12 @@ const ImageComponent = ({ image, imageName }) => {
         </button>
       </Box>
       <Modal open={open} onClose={handleClose}>
-        <Grid className="leader-info-grid" >
-          <Grid container alignItems="center" justify="flex-start" >
+        <Grid className="leader-info-grid">
+          <Grid container alignItems="center" justify="flex-start">
             <Grid xs={4}>
               <img className="leader-image-info" src={image} alt={imageName} />
             </Grid>
-            <Grid xs={8} justify="flex-start" >
+            <Grid xs={8} justify="flex-start">
               <Typography variant="h4" style={typographyWidth}>
                 {imageName}
               </Typography>
